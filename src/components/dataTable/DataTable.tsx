ae7763b2f30618dafe6bd579793fbc4f2a5aab81
Muhammad Azeem <abdulazeem5476@gmail.com>
2025-03-13 09:54:25 +0500

    Updated some features

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,8 +1,4 @@
-import {
-  DataGrid,
-  GridColDef,
-  GridToolbar,
-} from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
 import { Link } from "react-router-dom";
 // import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -14,7 +10,6 @@ type Props = {
 };
 
 const DataTable = (props: Props) => {
-
   // TEST THE API
 
   // const queryClient = useQueryClient();
@@ -29,10 +24,10 @@ const DataTable = (props: Props) => {
   // //   }
   // // });
 
-  const handleDelete = (id: number) => {
-    //delete the item
-    // mutation.mutate(id)
-  };
+  // const handleDelete = (id: number) => {
+  //   //delete the item
+  //   // mutation.mutate(id)
+  // };
 
   const actionColumn: GridColDef = {
     field: "action",
